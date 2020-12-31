692f49d9cfeda4ce10a96ec8974768fc273911a8
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-30 19:55:31 -0800

    Add repository

---
+++ Shabdakrida/Service/RiddleRepository.swift
@@ -0,0 +1,27 @@
+//
+//  RiddleRepository.swift
+//  Shabdakrida
+//
+//  Created by Pooja Chowdhary on 12/30/20.
+//
+
+import Foundation
+
+var repository: RiddleRepository!
+
+class RiddleRepository {
+    
+    var questionsList = [QuestionModel]()
+    var currentQuestionIndex: Int = 0
+    
+    init() {
+        let q1 = QuestionModel(imageUrl: "https://i.pinimg.com/originals/e1/dc/57/e1dc574a122ee009c1bb8571a4981c8b.png", options: ["मल्लक", "केशाः", "लोचन", "नखर"], answer: "लोचन")
+        let q2 = QuestionModel(imageUrl: "https://library.kissclipart.com/20190224/wwq/kissclipart-dog-clipart-puppy-maltese-dog-beagle-3400a99cc8df2422.png", options: ["श्वान", "बिडाला", "मूषक", "पीवन्"], answer: "श्वान")
+        
+        questionsList = [q1, q2]
+        currentQuestionIndex = 0
+    }
+    
+    
+    
+}
--- Shabdakrida/Shabdakrida/Models/QuestionModel.swift
+++ Shabdakrida/Shabdakrida/Models/QuestionModel.swift
@@ -23,9 +23,3 @@ class QuestionModel {
         self.answer = answer
     }
 }
-
-// Mock code
-fileprivate let q1 = QuestionModel(imageUrl: "https://i.pinimg.com/originals/e1/dc/57/e1dc574a122ee009c1bb8571a4981c8b.png", options: ["मल्लक", "केशाः", "लोचन", "नखर"], answer: "लोचन")
-fileprivate let q2 = QuestionModel(imageUrl: "https://library.kissclipart.com/20190224/wwq/kissclipart-dog-clipart-puppy-maltese-dog-beagle-3400a99cc8df2422.png", options: ["श्वान", "बिडाला", "मूषक", "पीवन्"], answer: "श्वान")
-
-let questions = [q1, q2]
