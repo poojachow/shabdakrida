7ff402eb80332bcfea459e2325649c9ca8b5f01c
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-30 16:35:08 -0800

    Update table view

--- Shabdakrida/Shabdakrida/ViewControllers/Level1ViewController.swift
+++ Shabdakrida/Shabdakrida/ViewControllers/Level1ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class Level1ViewController: UIViewController {
     
     let tableView = UITableView()
+    var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    let cellIdentifier = "optionCell"
     
     override func loadView() {
         super.loadView()
@@ -23,6 +25,28 @@ class Level1ViewController: UIViewController {
         tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
+        tableView.register(OptionTableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+}
+
+extension Level1ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! OptionTableViewCell
+        cell.textLabel?.text = characters[indexPath.row]
+        return cell
+    }
 }
