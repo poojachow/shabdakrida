b45cba362207de0f7199bb6d88f5c0e183450c76
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-31 13:21:47 -0800

    Get question list from Firebase and update cache

--- Shabdakrida/Shabdakrida/Service/RiddleRepository.swift
+++ Shabdakrida/Shabdakrida/Service/RiddleRepository.swift
@@ -18,34 +18,12 @@ class RiddleRepository {
     
     init() {
         ref = Database.database().reference()
-        let q1 = QuestionModel(imageUrl: "https://i.pinimg.com/originals/e1/dc/57/e1dc574a122ee009c1bb8571a4981c8b.png", options: ["मल्लक", "केशाः", "लोचन", "नखर"], answer: "लोचन")
-        let q2 = QuestionModel(imageUrl: "https://www.dogsinpictures.com/images/brown_white_clipart_dog_with_red_collar.jpg", options: ["श्वान", "बिडाला", "मूषक", "पीवन्"], answer: "श्वान")
-        
-        questionsList = [q1, q2]
+        questionsList.removeAll()
         currentQuestionIndex = 0
+        setupLevel1()
     }
     
     func getQuestion(isNext: Bool) -> QuestionModel? {
-        ref.child("level1").observeSingleEvent(of: .value) { (snapshot) in
-            if let data = snapshot.value as? [String: Any] {
-                print("######")
-                print(data["level"])
-                print(data["questions"])
-                if let questions = data["questions"] as? [Any] {
-                    for question in questions {
-                        print("######")
-                        if let question = question as? [String: Any] {
-                            print(question["answer"])
-                            print(question["image"])
-                            print(question["options"])
-                        }
-                    }
-                }
-            }
-        } withCancel: { (error) in
-            print("ERROR: No permission - \(error)")
-        }
-        
         if isNext {
             // Check if next is within range
             let nextIndex = currentQuestionIndex + 1
@@ -58,6 +36,30 @@ class RiddleRepository {
             }
         }
         return getQuestionFor(index: currentQuestionIndex)
+    }
+    
+    private func setupLevel1() {
+        ref.child("level1").observeSingleEvent(of: .value) { [weak self] (snapshot) in
+            if let data = snapshot.value as? [String: Any] {
+                print("######")
+                print(data["level"])
+                print(data["questions"])
+                if let questions = data["questions"] as? [Any] {
+                    for question in questions {
+                        print("######")
+                        if let question = question as? [String: Any] {
+                            if let answer = question["answer"] as? String, let image = question["image"] as? String, let options = question["options"] as? [String] {
+                                
+                                let questionModel = QuestionModel(imageUrl: image, options: options, answer: answer)
+                                self?.questionsList.append(questionModel)
+                            }
+                        }
+                    }
+                }
+            }
+        } withCancel: { (error) in
+            print("ERROR: No permission - \(error)")
+        }
     }
     
     private func getQuestionFor(index: Int) -> QuestionModel? {
