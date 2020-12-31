3b421de38fd0f49d4b367e4609b4d30e53e66ec7
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-30 16:19:48 -0800

    Add base tableview

--- Shabdakrida/Shabdakrida/ViewControllers/ViewController.swift
+++ Shabdakrida/Shabdakrida/ViewControllers/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func startClicked(_ sender: UIButton) {
+        let vc = Level1ViewController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
