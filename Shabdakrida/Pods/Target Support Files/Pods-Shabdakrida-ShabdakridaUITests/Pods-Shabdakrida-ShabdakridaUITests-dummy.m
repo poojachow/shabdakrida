f7cbc95f3d85dd77112ef37624d3a47fe4e3c6d9
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-30 20:14:34 -0800

    Install pods

---
+++ Shabdakrida/Pods/Target Support Files/Pods-Shabdakrida-ShabdakridaUITests/Pods-Shabdakrida-ShabdakridaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shabdakrida_ShabdakridaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Shabdakrida_ShabdakridaUITests
+@end
---
+++ Shabdakrida/Pods/Target Support Files/Pods-Shabdakrida/Pods-Shabdakrida-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shabdakrida : NSObject
+@end
+@implementation PodsDummy_Pods_Shabdakrida
+@end
