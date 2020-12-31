9e9ae83a0ebd7205ba9bdfee0d093204f2a0c0f1
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-31 12:56:51 -0800

    Add Firebase

--- Shabdakrida/Shabdakrida/Service/RiddleRepository.swift
+++ Shabdakrida/Shabdakrida/Service/RiddleRepository.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Firebase
 
 var repository: RiddleRepository!
 
@@ -13,8 +14,10 @@ class RiddleRepository {
     
     var questionsList = [QuestionModel]()
     var currentQuestionIndex: Int = 0
+    var ref: DatabaseReference
     
     init() {
+        ref = Database.database().reference()
         let q1 = QuestionModel(imageUrl: "https://i.pinimg.com/originals/e1/dc/57/e1dc574a122ee009c1bb8571a4981c8b.png", options: ["मल्लक", "केशाः", "लोचन", "नखर"], answer: "लोचन")
         let q2 = QuestionModel(imageUrl: "https://www.dogsinpictures.com/images/brown_white_clipart_dog_with_red_collar.jpg", options: ["श्वान", "बिडाला", "मूषक", "पीवन्"], answer: "श्वान")
         
@@ -23,6 +26,10 @@ class RiddleRepository {
     }
     
     func getQuestion(isNext: Bool) -> QuestionModel? {
+        ref.child("level1").observeSingleEvent(of: .value) { (snapshot) in
+            print("YAY!")
+        }
+        
         if isNext {
             // Check if next is within range
             let nextIndex = currentQuestionIndex + 1
