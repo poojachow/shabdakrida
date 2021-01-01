8c6dd529d60edb52c124852934f2c3cae59ae422
Pooja Chowdhary <poojachow172@gmail.com>
2020-12-31 19:17:47 -0800

    Add LevelOneViewController

---
+++ Shabdakrida/Shabdakrida/ViewControllers/LevelOneViewController.swift
@@ -0,0 +1,47 @@
+//
+//  LevelOneViewController.swift
+//  Shabdakrida
+//
+//  Created by Pooja Chowdhary on 12/31/20.
+//
+
+import UIKit
+
+class LevelOneViewController: UIViewController {
+
+    @IBOutlet weak var levelNameLabel: UILabel!
+    @IBOutlet weak var questionImageView: UIImageView!
+    @IBOutlet weak var option1Button: UIButton!
+    @IBOutlet weak var option2Button: UIButton!
+    @IBOutlet weak var option3Button: UIButton!
+    @IBOutlet weak var option4Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func option1Clicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func option2Clicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func option3Clicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func option4Clicked(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
